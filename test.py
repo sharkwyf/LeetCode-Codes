134b8b234bec0ce69db74ea6584146e62f297124
sharkwyf <122104004@qq.com>
2020-03-21 19:03:00 +0800

    Test Submit

--- test.py
+++ test.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
 # Filename: test.py
 from typing import List
-import threading
+import math
 
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        count = 0
+        t = math.ceil(math.log(n, 2) + 1)
+        arr = [pow(2, x) for x in range(0, t)]
+        while n > 1:
 
-class FooBar:
-    def __init__(self, n):
-        self.n = n
-        self.foolock = threading.Lock()
-        self.barlock = threading.Lock()
-        self.barlock.acquire()
+        return t
 
-    def foo(self, printFoo: 'Callable[[], None]') -> None:
-
-        for i in range(self.n):
-            self.foolock.acquire()
-            # printFoo() outputs "foo". Do not change or remove this line.
-            printFoo()
-            self.barlock.release()
-
-    def bar(self, printBar: 'Callable[[], None]') -> None:
-
-        for i in range(self.n):
-            self.barlock.acquire()
-            # printBar() outputs "bar". Do not change or remove this line.
-            printBar()
-            self.foolock.release()
-
-a = FooBar(10)
-_thread.start_new_thread(a.foo, )
-_thread.start_new_thread()
-
-
-
-# a = Solution()
-# b = a.accountsMerge(
-#     [["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["John", "johnsmith@mail.com", "john00@mail.com"],
-#      ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]]
-# )
-# print(b)
+a = Solution()
+b = a.integerReplacement(
+    24
+)
+print(b)
