1e82fcd70317959dfdf06361e48ed051c21f16fa
sharkwyf <122104004@qq.com>
2020-04-21 23:49:52 +0800

    test

--- C++/Test/ConsoleApplication1/main.cpp
+++ C++/Test/ConsoleApplication1/main.cpp
@@ -1,11 +1,6 @@
 #include "LeetCode.h"
 
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
  
 class Solution {
 public:
@@ -14,32 +9,36 @@ public:
 		unordered_map<int, ListNode*> dict;
 		unordered_map<int, ListNode*>::iterator it;
 		int last = 0;
-		while (head->val > 0) {
+		int index = 0;
+		while (head != NULL && head->val > 0) {
 			res.push_back(0);
-			if (last >= head->val) {
-
-			}
-			else {
+			if (last < head->val) {
 				vector<int> keys;
 				for (it = dict.begin(); it != dict.end(); it++) {
 					keys.push_back(it->first);
 				}
 				for (int k : keys) {
 					ListNode *p = dict[k];
-					while (res[p->val] == 0) {
-						if (p->val)
+					int id = k;
+					while (p != head && res[k] == 0) {
+						if (p->val < head->val)
 						{
-
+							res[k] = head->val;
 						}
+						p = p->next;
 						k++;
 					}
+					if (dict[id]->val < head->val)
+						dict.erase(id);
 				}
+				dict[index] = head;
 			}
 
 			last = head->val;
 			head = head->next;
+			index++;
 		}
-
+		return res;
 	}
 };
 
@@ -54,7 +53,22 @@ int main()
 		{3,2,5,4,6,1,7,0}
 	};
 
-	vector<int> res = s.loudAndRich(v1, vv);
+	ListNode *head = new ListNode(-1),
+		*n0 = new ListNode(2),
+		n1 = ListNode(1),
+		n2 = ListNode(5),
+		n3 = ListNode(3),
+		n4 = ListNode(5),
+		n5 = ListNode(0);
+	n0->next = &n1;
+	n1.next = &n2;
+	//n2.next = &n5;
+	//n2.next = &n3;
+	n3.next = &n4;
+	n4.next = &n5;
+
+
+	vector<int> res = s.nextLargerNodes(n0);
 
 	return 0;
 }
