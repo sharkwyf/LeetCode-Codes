50f2593e55f7f28483967bea5df1f6e38c33eee5
sharkwyf <122104004@qq.com>
2020-05-31 11:43:10 +0800

    test

--- Python/test.py
+++ Python/test.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python3
-# Filename: test.py
-import collections
-import math
-import random
-from functools import reduce
-from typing import List
-import re
+def encrypt(s, offset):
+    arr = list(s)
+    for i in range(len(arr)):
+        if "a" <= arr[i] <= "z":
+            arr[i] = chr(ord("A") + (ord(arr[i]) - ord("a") + offset) % 26)
+        elif "A" <= arr[i] <= "Z":
+            arr[i] = chr(ord("a") + (ord(arr[i]) - ord("A") + offset) % 26)
+        elif "0" <= arr[i] <= "9":
+            arr[i] = chr(ord("0") + (ord(arr[i]) - ord("0") + offset) % 26)
+    return "".join(arr)
 
 
-class Solution:
-    def minFallingPathSum(self, A: List[List[int]]) -> int:
-
-
-a = Solution()
-b = a.updateBoard(
-    [['B', '1', 'E', '1', 'B'],
-     ['B', '1', 'M', '1', 'B'],
-     ['B', '1', '1', '1', 'B'],
-     ['B', 'B', 'B', 'B', 'B']],
-    [1,2]
-)
-print(b)
+while True:
+    try:
+        s1 = input()
+        print(encrypt(s1, 1))
+        s2 = input()
+        print(encrypt(s2, -1))
+    except:
+        break
